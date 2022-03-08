1440d24805c4abedcd5637255fdc29ebe7eb843f
RobertsKamols <roberts.kamols@gmail.com>
2022-03-08 13:15:37 +0200

    stunda

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const vardi=['Olafs','Zintis','Georgs','Raitis','Guntis',];
 const balvas=['IPhone 3','5$ Top Gift Card','Xbox','Žurka','Kāmis',]
 let uzvaretajuSkaits =5 ;
 let rindas = document.querySelector('.rindas'); //pievieno mainīgo
-
+rindas.innerHTML='';
 
 
 
@@ -12,8 +12,13 @@ for (let i=0;i<uzvaretajuSkaits;i++){
   rand= Math.floor(rand); //noapaļo uz leju
   let uzvaretajs = vardi[rand];
   console.log(vardi[rand]);
-  rindas.innerHTML+=``;
+  let laimests = balvas[rand];
+  rindas.innerHTML+=`
+  <tr>
+    <td>${i+1}</td>
+    <td>${uzvaretajs}</td>
+    <td>${laimests}</td>
+  </tr>`
   
-}
-
+ }  
 
