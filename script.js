92778668a2ef5f5f5850ad0b2770b928d8ee4ba0
RobertsKamols <roberts.kamols@gmail.com>
2022-03-08 10:18:52 +0200

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const vardi=['Olafs','Zintis','Georgs','Raitis','Francis',];
+const balvas=['IPhone 3','5$ Top Gift Card','Xbox','Žurka','Kāmis',]
+let uzvaretajuSkaits =5 ;
+
+console.log(vardi[rand]);
+
